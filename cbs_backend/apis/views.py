f1ef1a17b672d498fac8f78941728c07891e5c66
Echodram <mardoche.kratos@gmail.com>
2024-12-04 14:34:04 +0100

    creating authencation views

--- cbs_backend/apis/views.py
+++ cbs_backend/apis/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.view import View
 from django.contrib.auth.models import User
 from .form import RegisterForm
 
