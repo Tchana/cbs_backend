f6b5b7d009a13da1783b73a7db5a89ddbbc086db
DaBe23 <davidberosy05@gmail.com>
2025-02-25 16:13:05 +0100

    Modified for more convinience with the backoffice

--- cbs_back/urls.py
+++ cbs_back/urls.py
@@ -16,8 +16,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from backend.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls'))
+    path('', home),
+    path('register', UserRegistrationView.as_view(), name='register'),
+    path('login', UserLoginView.as_view(), name='login'),
+    path('request_reset', RequestPasswordReset.as_view(), name='r_resetpwd'),
+    path('reset/<token>', ResetPassword.as_view(), name='r_resetpwd'),
+    
+    path('course/create', CreateCourseView.as_view(), name='c_course'),
+    path('course/get', GetListCourses.as_view(), name='g_course'),
+    path('course/edit/<uuid>', UpdateCourseView.as_view(), name='u_course'),
+    path('course/enroll', EnrollView.as_view(), name='subcourse'),
+    
+    path('lesson/add', AddLessonView.as_view(), name='c_lesson'),
+    path('lesson/get/<uuid>', GetLesson.as_view(), name='g_lesson'),
+    
+    path('user/', GetAllUserView.as_view(), name='g_user'),
+    path('user/me', GetMe.as_view(), name='g_p'),
+    path('user/<uuid>', DeleteUser.as_view(), name='d_user'),
+    
+    path('user/edit/<uuid>', EditUserProfileView().as_view(), name='e_user'),
+    path('user/teachers', GetTeacherView.as_view(), name='g_teacher'),
+    path('user/students', GetStudentView.as_view(), name='g_student'),
+    
+    path('book', AddBookView.as_view(), name="a_book"),
+    path('book', GetBookView.as_view({'get': 'list'}), name="g_book"),
+    
+    path('audio', AddAudioView.as_view(), name='a_audio'),
+    path('video', AddVideoView.as_view(), name='a_video') 
 ]
