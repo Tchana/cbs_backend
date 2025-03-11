6c62269e47f0707b804b89f03e2f582cd93995fb
Echodram <mardoche.kratos@gmail.com>
2025-03-11 15:43:24 +0100

    ....

--- backend/models.py
+++ backend/models.py
@@ -36,7 +36,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-
+    paid = models.BooleanField(default=False)
     ###Required fields###
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
