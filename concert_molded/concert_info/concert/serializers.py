e9a9ab9f678d5321fdf9a9baed6969ff622988be
Echodram <mardoche.kratos@gmail.com>
2025-06-26 17:46:04 +0100

    ...

--- concert_molded/concert_info/concert/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Information
-
-class InformationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Information
-        fields = "__all__"
